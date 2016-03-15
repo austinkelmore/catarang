3227ede97b7f1c763af8462233bdb682e82077b6
austinkelmore <github@austinkelmore.com>
2016-03-15 08:33:06 -0700

    Adding go-plugin and the basic plugin greeter (which will be removed later).

--- src/github.com/austinkelmore/catarang/main/main.go
+++ src/github.com/austinkelmore/catarang/main/main.go
@@ -3,18 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/austinkelmore/catarang/catarang"
+	"github.com/austinkelmore/catarang/greeter"
 	"github.com/austinkelmore/catarang/web"
 )
 
 func main() {
+	if len(os.Args) >= 2 && os.Args[1] != "" {
+		greeter.ClientPluginStart()
+		return
+	}
+
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	log.Println("Running Catarang!")
 	catarang.ReadInConfig()
 
 	r := web.CreateRoutes()
 	http.Handle("/", r)
+
+	greeter.ServerStart(os.Args)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- src/github.com/austinkelmore/catarang/scm/scm.go
+++ src/github.com/austinkelmore/catarang/scm/scm.go
@@ -1,6 +1,8 @@
 package scm
 
-import "github.com/austinkelmore/catarang/ulog"
+import (
+	"github.com/austinkelmore/catarang/ulog"
+)
 
 // SCMer interface for the source control modules
 type SCMer interface {
